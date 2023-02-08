9e4ec95e6646a21a90796b650465d03edaad17a8
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-08 20:44:58 +0100

    no puedo

---
+++ src/todo/mocks/characters.ts
@@ -0,0 +1,6 @@
+import { gotCard } from "../../features/about/components/gotcard/gotcard";
+import { gotCardStructure } from "../../features/about/models/gotcard";
+
+export const MOCK_CARDS: gotCardStructure[] = [
+  { name: "joaquin", family: "bezanilla", age: 27, state: true },
+];
